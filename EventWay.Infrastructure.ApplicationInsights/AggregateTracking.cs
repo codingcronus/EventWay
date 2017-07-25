dc3518c2c4fb0bd8cd495a5990ff96ecfe07c1e0
Chinh Dang <dtc@gt.miljoeportal.dk>
2017-07-25 16:25:55 +0700

    #908 - Use application insights to collect metrics for aggregates and events in data access layer

---
+++ EventWay.Infrastructure.ApplicationInsights/AggregateTracking.cs
@@ -0,0 +1,31 @@
+﻿using EventWay.Core;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+
+namespace EventWay.Infrastructure.ApplicationInsights
+{
+    public class AggregateTracking : IAggregateTracking
+    {
+        private readonly TelemetryClient _telemetryClient;
+
+        public AggregateTracking()
+        {
+            _telemetryClient = new TelemetryClient();
+        }
+
+        public void TrackEvents(IAggregate aggregate)
+        {
+            var evt = new EventTelemetry
+            {
+                Name = aggregate.GetType().Name
+            };
+
+            foreach (var @event in aggregate.GetUncommittedEvents())
+            {
+                evt.Properties.Add("EventType", @event.GetType().Name);
+            }
+
+            _telemetryClient.TrackEvent(evt);
+        }
+    }
+}
