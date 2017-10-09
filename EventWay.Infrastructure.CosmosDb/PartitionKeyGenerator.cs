4c4aebb0b4f0f6805be1e8f84bcf411775c8a9e5
Chinh Dang <dtc@globeteam.com>
2017-10-09 17:37:37 +0700

    Update PartitionKeyGenerator

--- EventWay.Infrastructure.CosmosDb/PartitionKeyGenerator.cs
+++ EventWay.Infrastructure.CosmosDb/PartitionKeyGenerator.cs
@@ -6,32 +6,16 @@ namespace EventWay.Infrastructure.CosmosDb
 {
     internal static class PartitionKeyGenerator
     {
-        private static MD5 _md5;
-        static PartitionKeyGenerator()
-        {
-            _md5 = MD5.Create();
-        }
-
         internal static string Generate(Guid id, int noOfPartitions)
         {
-            try
+            using (var md5 = MD5.Create())
             {
-                return TryToGenerate(id, noOfPartitions);
-            }
-            catch (ObjectDisposedException)
-            {
-                _md5 = MD5.Create();
-                return TryToGenerate(id, noOfPartitions);
-            }
-        }
+                var hashedValue = md5.ComputeHash(Encoding.UTF8.GetBytes(id.ToString()));
+                var asInt = BitConverter.ToInt32(hashedValue, 0);
+                asInt = asInt == int.MinValue ? asInt + 1 : asInt;
 
-        internal static string TryToGenerate(Guid id, int noOfPartitions)
-        {
-            var hashedValue = _md5.ComputeHash(Encoding.UTF8.GetBytes(id.ToString()));
-            var asInt = BitConverter.ToInt32(hashedValue, 0);
-            asInt = asInt == int.MinValue ? asInt + 1 : asInt;
-
-            return $"{Math.Abs(asInt) % noOfPartitions}";
+                return $"{Math.Abs(asInt) % noOfPartitions}";
+            }
         }
     }
 }
