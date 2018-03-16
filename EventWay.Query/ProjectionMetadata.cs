abca9e98391000aa3b1e2431b0748d4e4a0dada5
Kristian Lindberg Vinther <kristian.vinther@gmail.com>
2018-03-16 23:58:20 +0100

    Added ProjectionType to ProjectionMetadata (as optional).

--- EventWay.Query/ProjectionMetadata.cs
+++ EventWay.Query/ProjectionMetadata.cs
@@ -6,13 +6,16 @@ namespace EventWay.Query
     {
         public ProjectionMetadata(
             Guid projectionId,
-            long eventOffset)
+            long eventOffset,
+            string projectionType = null)
         {
             ProjectionId = projectionId;
             EventOffset = eventOffset;
+            ProjectionType = projectionType;
         }
 
         public Guid ProjectionId { get; set; }
         public long EventOffset { get; set; }
+        public string ProjectionType { get; set; }
     }
 }
